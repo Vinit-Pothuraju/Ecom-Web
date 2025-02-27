d9441a440c418538994fd421280365082597572f
Vinit-Pothuraju <vinitkumarpothuraju@gmail.com>
2025-02-27 16:07:09 +0530

    inital commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Routes ,Route} from 'react-router-dom'
+import { Home, Collections, Product, Contact, About, Cart, Login, PlaceOrder, Orders,Profile} from './pages';
+
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/Collections' element={<Collections/>} />
+        <Route path='/About' element={<About/>} />
+        <Route path='/Contact' element={<Contact/>} />
+        <Route path='/Product/:productId' element={<Product/>} />
+        <Route path='/Cart' element={<Cart/>} />
+        <Route path='/Login' element={<Login/>} />
+        <Route path='/PlaceOrder' element={<PlaceOrder/>} />
+        <Route path='/Orders' element={<Orders/>} />
+        <Route path='/Profile' element={<Profile/>} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ Frontend/src/pages/index.js
@@ -0,0 +1,10 @@
+export { default as Home } from './Home';
+export { default as Collections } from './Collections';
+export { default as Product } from './Product';
+export { default as Contact } from './Contact';
+export { default as About } from './About';
+export { default as Cart } from './Cart';
+export { default as Login } from './Login';
+export { default as PlaceOrder } from './PlaceOrder';
+export { default as Orders } from './Orders';
+export { default as Profile } from './Profile';
